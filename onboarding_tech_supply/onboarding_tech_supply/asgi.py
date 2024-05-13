16bc57aeb59254ca2974a1815796075dfed2fcea
Kleber Curvelo <klebercurvelo@arcoeducacao.com.br>
2024-05-13 16:14:24 -0300

    starting django project

---
+++ onboarding_tech_supply/onboarding_tech_supply/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for onboarding_tech_supply project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onboarding_tech_supply.settings')
+
+application = get_asgi_application()
---
+++ onboarding_tech_supply/onboarding_tech_supply/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for onboarding_tech_supply project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onboarding_tech_supply.settings')
+
+application = get_wsgi_application()
