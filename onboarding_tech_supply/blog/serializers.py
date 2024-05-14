977492dc0c59efc6300f646c59c98721048a4590
Kleber Curvelo <klebercurvelo@arcoeducacao.com.br>
2024-05-14 15:26:57 -0300

    adds the add post view tests

--- onboarding_tech_supply/blog/serializers.py
+++ onboarding_tech_supply/blog/serializers.py
@@ -5,3 +5,4 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ['id', 'title', 'content', 'autor', 'created_at']
+        extra_kwargs = {'autor': {'required': False}}
--- onboarding_tech_supply/blog/tests.py
+++ onboarding_tech_supply/blog/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Post
 
 # Create your tests here.
+
+class PostListTests(APITestCase):
+    def test_add_post(self):
+        url = reverse('post-list')
+        data = {'title': 'Lorem Ipsum test', 'content': 'Lorem ipsum dolor sit amet'}
+        response = self.client.post(url, data, format='json')
+        
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Post.objects.count(), 1)
+        self.assertEqual(Post.objects.get().title, 'Lorem Ipsum test')
+    
+    def test_add_invalid_post(self):
+        url = reverse('post-list')
+        data = {'content': 'Lorem ipsum dolor sit amet'}
+        response = self.client.post(url, data, format='json')
+        
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
